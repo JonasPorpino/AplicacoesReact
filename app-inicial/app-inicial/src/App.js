940062a9d2584a044723dc7c2c76485ace69f8a3
JonasPorpino <49802323+JonasPorpino@users.noreply.github.com>
2024-06-14 14:57:33 -0300

    Update App.js

--- app-inicial/app-inicial/src/App.js
+++ app-inicial/app-inicial/src/App.js
@@ -1,14 +1,14 @@
 export default function App() {
 
   const tarefa = {id: "T1", descricao:"Estudar React", conluida: false};
- /**
+ 
   if(tarefa.conluida){
     return <p>Tarefa conluída: {tarefa.descricao}</p>
   } else{
     return <p>Tarefa pendente: {tarefa.descricao}</p>
   }
 
-
+}
 
 /** sintaxe mais limpar 
   return<p>
@@ -19,8 +19,7 @@ export default function App() {
 /** para o caso de uma
   return (tarefa.conluida) ? null : <p>tarefa pendente: {tarefa.descricao}</p>;
 
-*/
+
 
   return tarefa.conluida && <p>Tarefa concluida: {tarefa.descricao}</p>;
-
-}
+*/
